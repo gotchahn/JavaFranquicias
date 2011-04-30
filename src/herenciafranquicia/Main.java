167050b87ee8ec022e8b577e9b762ac4ca47d5d8
Carlos Gochez <carlos.gochez@unitec.edu>
2011-04-29 18:24:21 -0600

    cambios hechos el jueves 28 abr

--- src/herenciafranquicia/Main.java
+++ src/herenciafranquicia/Main.java
@@ -1,9 +1,11 @@
 /*
- * To change this template, choose Tools | Templates
+ * To change this template, choose Tools |\Templates
  * and open the template in the editor.
  */
 
 package herenciafranquicia;
+
+import GrupoJavaFood.*;
 
 /**
  *
@@ -16,6 +18,9 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //UPCASTING
+        Franquicia fb = new BurguerKing("BK Sta Monica");
+        System.out.println("pagar: " + fb.calcularTotalFinal(100));
     }
 
 }
